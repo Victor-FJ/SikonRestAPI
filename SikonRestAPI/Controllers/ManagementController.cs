033b8f1e4d117316592ccef44068930833c1e07d
Victor Friis-Jensen <vict5481@edu.easj.dk>
2020-04-20 12:47:18 +0200

    Management classes

---
+++ SikonRestAPI/Controllers/ManagementController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SikonRestAPI.SQLUtility;
+
+namespace SikonRestAPI.Controllers
+{
+    public class ManagementController : ApiController
+    {
+        //Posts the connectionstring
+        //POST: api/Manage
+        [HttpPost]
+        [Route("api/Manage/")]
+        public bool Post([FromBody] string connectionString)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    ManagementUtil.ConnectionString = connectionString;
+                    return true;
+                }
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+    }
+}
