fe218dfad3d3d9418916ce2595fb4e7ae311e387
Victor Friis-Jensen <vict5481@edu.easj.dk>
2020-04-27 09:41:16 +0200

    EventManager Changes

---
+++ SikonRestAPI/Controllers/EventController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ModelLibrary.Model;
+
+namespace SikonRestAPI.Controllers
+{
+    public class EventController : ApiController
+    {
+        // GET: api/Event
+        public IEnumerable<Event> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Event/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Event
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Event/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Event/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SikonRestAPI/Controllers/ImageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SikonRestAPI.SQLUtility;
+
+namespace SikonRestAPI.Controllers
+{
+    public class ImageController : ApiController
+    {
+        private static readonly ImageManager Manager = new ImageManager();
+
+        //Gets all image names
+        //GET: api/Image
+        [HttpGet]
+        [Route("api/Image")]
+        public IEnumerable<string> GetNames()
+        {
+            return Manager.GetNames();
+        }
+
+        //Gets requested images
+        //GET: api/Image/{name}
+        [HttpGet]
+        [Route("api/Image/{name}")]
+        public byte[] Get(string name)
+        {
+            byte[] test = Manager.Get(name);
+            return test;
+        }
+
+        [HttpPost]
+        [Route("api/Image/{name}")]
+        public bool Post(string name, [FromBody] byte[] pixelBytes)
+        {
+            return Manager.Post(name, pixelBytes);
+        }
+
+        [HttpDelete]
+        [Route("api/Image/{name}")]
+        public bool Delete(string name)
+        {
+            return Manager.Delete(name);
+        }
+    }
+}
