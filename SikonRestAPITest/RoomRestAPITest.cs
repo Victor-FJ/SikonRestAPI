8b3431b1110b35b9b396f1658ea18dd710e9e3ff
Nicolai Höyer Christiansen <nico388a@edu.easj.dk>
2020-05-18 10:01:28 +0200

    lavet en unittest

--- SikonRestAPITest/RoomRestAPITest.cs
+++ SikonRestAPITest/RoomRestAPITest.cs
@@ -10,40 +10,39 @@ namespace SikonRestAPITest
     [TestClass]
    public class RoomRestApiTest
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        
+        [TestMethod]
+        public void RoomtestPostTest()
+        {
+            //Arrange
+            ManagementUtil.ConnectionString = "Data Source = nicolaiserver.database.windows.net; Initial Catalog = NicolaiDataBase; User ID = NicolaiAdmin; Password = Seacret1234; Connect Timeout = 30; Encrypt = True; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False;";
+            ManageRoom manageRoom = new ManageRoom();
+            Room newRoom = new Room("6A", "Bulevar 66", 20);
+
+            //Act
+            bool ok = manageRoom.Post(newRoom);
+            manageRoom.Delete("6A");
+
+            //Assert
+            Assert.AreEqual(true, ok);
+        }
+
         //[TestMethod]
-        //public void RoomtestGetOne()
+        //public void RoomtestDelete()
         //{
         //    //Arrange
         //    ManageRoom manageRoom = new ManageRoom();
         //    Room newRoom = new Room("6A", "Bulevar 66", 20);
-
+        //    List<Room> rooms = manageRoom.Get().ToList();
+        //    int antalBeforeDelete = rooms.Count;
         //    //Act
-        //    bool ok = manageRoom.PostRoom(newRoom);
-        //    manageRoom.Delete("6A");
+        //    bool ok = manageRoom.Post(newRoom);
+        //    rooms = manageRoom.Get().ToList();
+        //    int antalAfterDelete = rooms.Count;
 
         //    //Assert
         //    Assert.AreEqual(true, ok);
+        //    Assert.AreEqual(antalBeforeDelete -1, antalAfterDelete);
         //}
-
-        [TestMethod]
-        public void RoomtestDelete()
-        {
-            //Arrange
-            ManageRoom manageRoom = new ManageRoom();
-            Room newRoom = new Room("6A", "Bulevar 66", 20);
-            List<Room> rooms = manageRoom.Get().ToList();
-            int antalBeforeDelete = rooms.Count;
-            //Act
-            bool ok = manageRoom.Post(newRoom);
-            rooms = manageRoom.Get().ToList();
-            int antalAfterDelete = rooms.Count;
-
-            //Assert
-            Assert.AreEqual(true, ok);
-            Assert.AreEqual(antalBeforeDelete -1, antalAfterDelete);
-        }
     }
 }
